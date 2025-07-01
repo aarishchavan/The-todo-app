a89439c25f0b4305ebf697843be369fd95b73de6
aarishchavan <163218386+aarishchavan@users.noreply.github.com>
2025-07-01 13:49:42 -0400

    reordered the entry bar

--- web.py
+++ web.py
@@ -15,6 +15,9 @@ st.title("My Todo App")
 st.subheader("This is my todo app")
 st.write("This app is to increase your productivity")
 
+st.text_input(label = "", placeholder = "Add a new todo...",
+              on_change=add_todo, key = 'new_todo')
+
 for index, todo in enumerate(todos.copy()):
     checkbox = st.checkbox(todo.strip(), key=f"todo_{index}")
     if checkbox:
@@ -26,5 +29,4 @@ for index, todo in enumerate(todos.copy()):
             st.rerun()
 
 
-st.text_input(label = "", placeholder = "Add a new todo...",
-              on_change=add_todo, key = 'new_todo')
+
