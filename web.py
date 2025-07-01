4c2fe2b0838b046f6468a29e6a9c0cd452f09a49
aarishchavan <163218386+aarishchavan@users.noreply.github.com>
2025-07-01 12:03:02 -0400

    Intial commit

---
+++ web.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import functions
+
+todos = functions.get_todos()
+
+def add_todo():
+    todo = st.session_state["new_todo"]+ "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
+# streamlit run C:\Users\anish\PycharmProjects\web_app1\web.py [ARGUMENTS]
+
+st.title("My Todo App")
+st.subheader("This is my todo app")
+st.write("This app is to increase your productivity")
+
+for index, todo in enumerate(todos.copy()):
+    checkbox = st.checkbox(todo.strip(), key=f"todo_{index}")
+    if checkbox:
+        # Get fresh todos list and remove the item
+        current_todos = functions.get_todos()
+        if index < len(current_todos):
+            current_todos.pop(index)
+            functions.write_todos(current_todos)
+            st.rerun()
+
+st.text_input(label = "", placeholder = "Add a new todo...",
+              on_change=add_todo, key = 'new_todo')
